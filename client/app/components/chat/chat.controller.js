408b5ceda7d470cc960dd036fd6452700df37575
Beliars <2beliars@gmail.com>
2017-01-31 23:32:15 +0200

    styles changes

--- client/app/components/chat/chat.controller.js
+++ client/app/components/chat/chat.controller.js
@@ -1,40 +1,41 @@
 class ChatController {
-  constructor(Restangular, BotService) {
-    this.name = 'home';
-    this.botService = BotService;
-    this.restangular = Restangular;
-    this.messages = [];
-    this.index = 0;
-    this.gender = {
-      text: '',
-      user: true
-    };
-    this.love = {
-        text: '',
-        user: true
-    };
-    this.problem = {
-        text: '',
-        user: true
-    };
-  }
-
-  $onInit() {
-    for(let i=0; i<3; i++) {
-        this.messages.push(this.botService.questions[i]);
-        this.index = i;
+    constructor(Restangular, BotService, $timeout) {
+        this.name = 'home';
+        this.botService = BotService;
+        this.restangular = Restangular;
+        this.$timeout = $timeout;
+        this.messages = [];
+        this.index = 0;
+        this.gender = {
+            text: '',
+            user: true
+        };
+        this.love = {
+            text: '',
+            user: true
+        };
+        this.problem = {
+            text: '',
+            user: true
+        };
     }
-  }
 
-  answer() {
-    this.index += 1;
-    if(this.index < this.botService.questions.length) {
-        this.messages.push(this.botService.questions[this.index]);
+    $onInit() {
+        for (let i = 0; i < 3; i++) {
+            this.messages.push(this.botService.questions[i]);
+            this.index = i;
+        }
     }
-  }
+
+    answer() {
+        this.index += 1;
+        if (this.index < this.botService.questions.length) {
+            this.messages.push(this.botService.questions[this.index]);
+        }
+    }
 
     onSubmitGender(form) {
-        if(form.$valid) {
+        if (form.$valid) {
             this.messages.push(this.gender);
             this.addNewQuestion();
             console.log(this.messages);
@@ -42,24 +43,33 @@ class ChatController {
     }
 
     onSubmitLove(form) {
-        if(form.$valid) {
+        if (form.$valid) {
             this.messages.push(this.love);
             this.addNewQuestion();
         }
     }
 
     onSubmitProblem(form) {
-        if(form.$valid) {
+        if (form.$valid) {
             this.messages.push(this.problem);
             this.addNewQuestion();
         }
     }
 
     addNewQuestion() {
-        this.index += 1;
-        if (this.index < this.botService.questions.length) {
-            this.messages.push(this.botService.questions[this.index]);
-        }
+        var elem = document.getElementsByClassName('chat-section')[0];
+        this.$timeout(() => {
+            this.index += 1;
+            if (this.index < this.botService.questions.length) {
+                this.messages.push(this.botService.questions[this.index]);
+                elem.scrollTop = elem.scrollHeight;     
+            }                                                                
+        }, 2000); // here we can use promise and .then after resolve
+
+        this.$timeout(() => {
+            var elem = document.getElementsByClassName('chat-section')[0];
+            elem.scrollTop = elem.scrollHeight;     
+        }, 2010);
     }
 }
 
